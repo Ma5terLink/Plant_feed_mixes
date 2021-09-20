13eb1881ed3a96c561a1343c9414dac9aa234cfe
Ma5terlink <devalon@yandex.ru>
2021-09-20 19:40:12 +0500

    01 - Основное меню, гамбургер. Main menu, hamburger

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -14,8 +14,7 @@ $(window).scroll(function () {
 $("a[href^='#up']").click(function () {
     var _href = $(this).attr("href");
     $("html, body").animate({scrollTop: '0'});
-    /* КАК РАБОТАЕТ ФУНКЦИЯ НИЖЕ - ПОКА НЕ ПОНЯЛ..
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"}); */
+    //$("html, body").animate({scrollTop: $(_href).offset().top+"px"}); */
     return false;
 });
 
